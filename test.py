2aa02e564a07104e4ff5ea69de180bd10749ac5f
Alistair Keiller <alistair@keiller.net>
2024-02-22 16:16:43 -0800

    update

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ def inject_dark_mode(debugger_url):
                 if (typeof DarkReader === 'undefined') {
                     var script = document.createElement('script');
                     script.onload = function() {
-                        DarkReader.enable();
+                        DarkReader.enable({darkSchemeBackgroundColor: "#1e1e2e", darkSchemeTextColor: "	#cdd6f4", selectionColor: "#585b70"});
                         document.body.setAttribute('darkreader-injected', 'true');
                     };
                     script.src = 'https://unpkg.com/darkreader@4.9.77/darkreader.js';
@@ -35,13 +35,12 @@ def inject_dark_mode(debugger_url):
 
             ws = websocket.create_connection(ws_url)
             ws.send(command)
-            response = ws.recv()  # Receive server response
+            response = ws.recv()
             print(f"Injected DarkReader for page ID {page_id}: {response}")
             ws.close()
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# Continuous service loop
 while True:
     inject_dark_mode("http://localhost:9222/json")
-    time.sleep(1)  # Wait for 60 seconds before checking again
+    time.sleep(1)
