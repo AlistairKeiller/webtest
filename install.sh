7f61a9de922e0e8db4374eac3c212551d71d2d62
Alistair Keiller <alistair@keiller.net>
2024-02-23 22:24:14 -0800

    add service

--- install.sh
+++ install.sh
@@ -3,4 +3,25 @@ mkdir -p "/Applications/Adobe Connect Dark Mode.app/Contents/MacOS"
 echo """#!/bin/sh
 /Applications/Adobe\ Connect/Adobe\ Connect.app/Contents/MacOS/Adobe\ Connect --remote-debugging-port=9222""" > "/Applications/Adobe Connect Dark Mode.app/Contents/MacOS/Adobe Connect Dark Mode"
 chmod +x "/Applications/Adobe Connect Dark Mode.app/Contents/MacOS/Adobe Connect Dark Mode"
-chmod +x test.py
+echo """<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\">
+<dict>
+    <key>Label</key>
+    <string>com.alistair.webtest</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/usr/bin/python3</string>
+        <string>$(pwd)/test.py</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>KeepAlive</key>
+    <true/>
+    <key>StandardErrorPath</key>
+    <string>/tmp/com.alistair.webtest.err</string>
+    <key>StandardOutPath</key>
+    <string>/tmp/com.alistair.webtest.out</string>
+</dict>
+</plist>""" > com.alistair.webtest.plist
+launchctl load com.alistair.webtest.plist
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 rm -r "/Applications/Adobe Connect Dark Mode.app"
+rm com.alistair.webtest.plist
+launchctl unload com.alistair.webtest.plist
