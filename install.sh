7e9f54b4c615acfad5476c17d5aa38503a506ac8
Alistair Keiller <alistair@keiller.net>
2024-02-23 22:10:08 -0800

    add service

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ mkdir -p "/Applications/Adobe Connect Dark Mode.app/Contents/MacOS"
 echo """#!/bin/sh
 /Applications/Adobe\ Connect/Adobe\ Connect.app/Contents/MacOS/Adobe\ Connect --remote-debugging-port=9222""" > "/Applications/Adobe Connect Dark Mode.app/Contents/MacOS/Adobe Connect Dark Mode"
 chmod +x "/Applications/Adobe Connect Dark Mode.app/Contents/MacOS/Adobe Connect Dark Mode"
+chmod +x test.py
